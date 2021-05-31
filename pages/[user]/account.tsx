6bf110ec7bad24384ae98afb5f04b3cd4abb94dd
angeloreborn <angelowolffreborn@gmail.com>
2021-05-31 16:48:05 +0200

    Create account.tsx

---
+++ pages/[user]/account.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+function Account(props: Props) {
+    const {} = props
+
+    return (
+       <h1>Account</h1> 
+    )
+}
+
+export default Account
