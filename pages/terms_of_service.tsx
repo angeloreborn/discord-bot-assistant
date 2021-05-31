994b9e34f87532ca44f77784d809646093d5a409
angeloreborn <angelowolffreborn@gmail.com>
2021-05-31 16:47:41 +0200

    Create terms_of_service.tsx

---
+++ pages/terms_of_service.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+function Terms_of_service(props: Props) {
+    const {} = props
+
+    return (
+        <h1>Terms of Service</h1>
+    )
+}
+
+export default Terms_of_service
