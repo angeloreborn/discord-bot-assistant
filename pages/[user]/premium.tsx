6c79ff92ada229db0217405b67f5dd6e23f87694
angeloreborn <angelowolffreborn@gmail.com>
2021-05-31 17:09:30 +0200

    Update premium.tsx

---
+++ pages/[user]/premium.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+function Premium(props: Props) {
+    const {} = props
+
+    return (
+        <h1>Premium</h1>
+    )
+}
+
+export default Premium
