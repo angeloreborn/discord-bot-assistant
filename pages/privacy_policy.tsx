1b0f17daaee553d3cb9e895ac8f2a5f8a743016c
angeloreborn <angelowolffreborn@gmail.com>
2021-05-31 16:47:01 +0200

    Create privacy_policy.tsx

---
+++ pages/privacy_policy.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+function Privacy_policy(props: Props) {
+    const {} = props
+
+    return (
+        <h1>Privacy Policy</h1>
+    )
+}
+
+export default Privacy_policy
