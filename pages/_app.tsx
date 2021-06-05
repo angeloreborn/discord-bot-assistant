0e5bc7cf59b3b06f9321d8986490215c7a1a86e0
angeloreborn <angelowolffreborn@gmail.com>
2021-06-05 18:15:36 +0200

    Set viewport head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/navbar.css'
 import '../styles/notification.css'
 import '../styles/dashboard.css'
 import '../styles/loader.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 import SideNav from '../components/Navbar/NavBar'
@@ -12,6 +13,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
   <Dashboard>
     <SideNav />
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    </Head>
     <section className='content-viewport-width'>
       <TopBar></TopBar>
      <Component {...pageProps} />
